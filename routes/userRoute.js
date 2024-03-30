a74dcbbb6adc1572a6ae9869dd2230d9fddeb432
Shahanaz_Akter <shahanazakterm1999@gmail.com>
2024-03-30 13:51:58 +0600

    ajker code

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { userView, master, category, termsCondition } = require('../controllers/userController');
+const { userView, master, category, termsCondition, privacyPolicy } = require('../controllers/userController');
 
 
 router.get('/', userView);
@@ -10,6 +10,7 @@ router.get('/master', master);
 router.get('/grocery', category);
 
 router.get('/terms_condition', termsCondition);
+router.get('/privacy_policy', privacyPolicy);
 
 
 module.exports = router;
