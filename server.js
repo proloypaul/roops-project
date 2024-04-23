71617666aa1eacad8933e52362f13102e6df9787
proloypaul <proloyp23@gmail.com>
2024-04-23 17:34:53 +0600

    set build command

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
-    console.log('Connected to MongoDB');
+    // console.log('Connected to MongoDB');
 });
 
 // mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{ 
